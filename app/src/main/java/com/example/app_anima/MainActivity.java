bc58244fa3dbce9f7faea461043ff0e438e63423
YoonGyeong <14dnfnfn@gmail.com>
2020-09-29 03:24:40 +0900

    초기 설정

--- app/src/main/java/com/example/app_anima/MainActivity.java
+++ app/src/main/java/com/example/app_anima/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.app_anima;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
