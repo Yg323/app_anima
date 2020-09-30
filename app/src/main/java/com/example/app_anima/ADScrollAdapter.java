eabe693d2aba0dbb6772bc29d2a13c41cc816afc
YoonGyeong <14dnfnfn@gmail.com>
2020-09-30 23:01:30 +0900

    1. 오래된 라이브러리로 뷰페이저 구현 완료
    앞으로 수정할 일
    1. 뷰페이저 다른 방식으로 구현하기
    2. 뷰페이저 layout으로 감싸기 (스크롤뷰 체크 위함)

---
+++ app/src/main/java/com/example/app_anima/ADScrollAdapter.java
@@ -0,0 +1,56 @@
+package com.example.app_anima;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+public class ADScrollAdapter extends PagerAdapter {
+
+    Context context;
+    ArrayList<Drawable> data;
+
+    public ADScrollAdapter(Context context, ArrayList<Drawable> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        //뷰페이지 슬라이딩 할 레이아웃 인플레이션
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.ad_viewpager,null);
+        ImageView image_container = (ImageView) v.findViewById(R.id.image_container);
+        Glide.with(context).load(data.get(position)).into(image_container);
+        container.addView(v);
+        return v;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((View)object);
+
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/example/app_anima/FragmentHome.java
+++ app/src/main/java/com/example/app_anima/FragmentHome.java
@@ -1,16 +1,78 @@
 package com.example.app_anima;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
+
 public class FragmentHome extends Fragment {
+    private Button btn_test;
+    private ImageButton btn_menu;
+    private ScrollView scrollView;
+    private LinearLayout linearLayout;
+    private TextView tv_menu;
+
+    private AutoScrollViewPager autoViewPager;
+    private ArrayList<Drawable> mList;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+
+        btn_test = (Button) viewGroup.findViewById(R.id.button2);
+        btn_menu = (ImageButton) viewGroup.findViewById(R.id.btn_menu);
+        scrollView = (ScrollView) viewGroup.findViewById(R.id.sv_main);
+        linearLayout = (LinearLayout) viewGroup.findViewById(R.id.appbar);
+        tv_menu = (TextView) viewGroup.findViewById(R.id.tv_menu);
+        autoViewPager = viewGroup.findViewById(R.id.autoViewPager);
+
+        mList = new ArrayList<Drawable>();
+        mList.add(ResourcesCompat.getDrawable(getResources(),R.drawable.img_ad1,null));
+        mList.add(ResourcesCompat.getDrawable(getResources(),R.drawable.img_ad2,null));
+        mList.add(ResourcesCompat.getDrawable(getResources(),R.drawable.img_ad3,null));
+        mList.add(ResourcesCompat.getDrawable(getResources(),R.drawable.img_ad4,null));
+
+
+        ADScrollAdapter adScrollAdapter = new ADScrollAdapter(getContext(),mList);
+        autoViewPager.setAdapter(adScrollAdapter); //Auto Viewpager에 Adapter 장착
+        autoViewPager.setInterval(5000); // 페이지 넘어갈 시간 간격 설정
+        autoViewPager.startAutoScroll(); //Auto Scroll 시작
+
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                int scrollY = scrollView.getScrollY(); // For ScrollView
+                int scrollX = scrollView.getScrollX(); // For HorizontalScrollView
+                if(scrollY>=btn_test.getBottom()){
+                    linearLayout.setBackgroundColor(Color.parseColor("#E7D0C8"));
+                    tv_menu.setVisibility(View.VISIBLE);
+                    btn_menu.setColorFilter(Color.parseColor("#000000"));
+                }
+                else {
+                    linearLayout.setBackgroundColor(Color.parseColor("#00E7D0C8"));
+                    tv_menu.setVisibility(View.INVISIBLE);
+                    btn_menu.setColorFilter(Color.parseColor("#FFFFFF"));
+                }
+            }
+        });
+        return viewGroup;
     }
+
+
 }
