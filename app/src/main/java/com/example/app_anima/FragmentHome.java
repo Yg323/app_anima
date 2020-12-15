685a96f633c8005e8e1f711ec20e773397eaf586
leesh96 <suho2718@gmail.com>
2020-12-15 17:43:56 +0900

    20.12.15 17:43 사료 입력 구현 예외 처리 안했음

--- app/src/main/java/com/example/app_anima/FragmentHome.java
+++ app/src/main/java/com/example/app_anima/FragmentHome.java
@@ -1,5 +1,6 @@
 package com.example.app_anima;
 
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -9,11 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.res.ResourcesCompat;
@@ -31,6 +37,8 @@ import java.util.TimerTask;
 public class FragmentHome extends Fragment {
 
     private ImageButton btn_menu,circle_add,circle_minus;
+    private ImageButton btn_feedinput; // 사료 입력 버튼 선언
+    private TextView tv_calorie;
     private ScrollView scrollView;
     private LinearLayout appbar, vp_layout;
     private TextView tv_menu,tv_water;
@@ -50,7 +58,8 @@ public class FragmentHome extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
-
+        btn_feedinput = (ImageButton) viewGroup.findViewById(R.id.btn_feedinput); // 사료 입력 버튼
+        tv_calorie = (TextView) viewGroup.findViewById(R.id.calory);
         btn_menu = (ImageButton) viewGroup.findViewById(R.id.btn_menu);
         circle_add = (ImageButton) viewGroup.findViewById(R.id.circle_add);
         circle_minus = (ImageButton) viewGroup.findViewById(R.id.circle_minus);
@@ -133,6 +142,126 @@ public class FragmentHome extends Fragment {
             }
         });
 
+        //사료
+        btn_feedinput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final double curCal;
+                if (tv_calorie.getText().toString().equals(null)) {
+                    curCal = 0;
+                } else {
+                    curCal = Double.parseDouble(tv_calorie.getText().toString());
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_food, null, false);
+                builder.setView(view);
+
+                final RadioGroup mRadioGroup;
+                final RadioButton radioButtonFood, radioButtonSnack;
+                final EditText editTextInputFeed;
+                final Button btnPlus100, btnPlus10, btnPlus1, btnMinus100, btnMinus10, btnMinus1;
+                final EditText editTextInputCal;
+                final Button btnApply, btnCancel;
+                final boolean isFood;
+
+                mRadioGroup = (RadioGroup) view.findViewById(R.id.radiocategory);
+                radioButtonFood = (RadioButton) view.findViewById(R.id.radiofood);
+                radioButtonSnack = (RadioButton) view.findViewById(R.id.radiosnack);
+                editTextInputFeed = (EditText) view.findViewById(R.id.input_feed);
+                btnPlus100 = (Button) view.findViewById(R.id.btn_plus100);
+                btnPlus10 = (Button) view.findViewById(R.id.btn_plus10);
+                btnPlus1 = (Button) view.findViewById(R.id.btn_plus1);
+                btnMinus100 = (Button) view.findViewById(R.id.btn_minus100);
+                btnMinus10 = (Button) view.findViewById(R.id.btn_minus10);
+                btnMinus1 = (Button) view.findViewById(R.id.btn_minus1);
+                editTextInputCal = (EditText) view.findViewById(R.id.input_calorie);
+                btnApply = (Button) view.findViewById(R.id.btn_apply);
+                btnCancel = (Button) view.findViewById(R.id.btn_cancel);
+
+                RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                    }
+                };
+                mRadioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
+                radioButtonFood.setChecked(true);
+
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                btnPlus100.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Double curInputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        curInputFeed += 100;
+                        editTextInputFeed.setText(String.valueOf(curInputFeed));
+                    }
+                });
+                btnPlus10.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Double curInputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        curInputFeed += 10;
+                        editTextInputFeed.setText(String.valueOf(curInputFeed));
+                    }
+                });
+                btnPlus1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Double curInputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        curInputFeed += 1;
+                        editTextInputFeed.setText(String.valueOf(curInputFeed));
+                    }
+                });
+                btnMinus100.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Double curInputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        curInputFeed -= 100;
+                        editTextInputFeed.setText(String.valueOf(curInputFeed));
+                    }
+                });
+                btnMinus10.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Double curInputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        curInputFeed -= 10;
+                        editTextInputFeed.setText(String.valueOf(curInputFeed));
+                    }
+                });
+                btnMinus1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Double curInputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        curInputFeed -= 1;
+                        editTextInputFeed.setText(String.valueOf(curInputFeed));
+                    }
+                });
+
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+
+                btnApply.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int checkedRadio = mRadioGroup.getCheckedRadioButtonId();
+                        double inputCalorie = Double.parseDouble(editTextInputCal.getText().toString());
+                        double calculatedCalorie = inputCalorie / 100;
+                        double inputFeed = Double.parseDouble(editTextInputFeed.getText().toString());
+                        double finalFeed = inputFeed * calculatedCalorie;
+                        tv_calorie.setText(String.format("%.2f", finalFeed + curCal));
+                        alertDialog.dismiss();
+                    }
+                });
+            }
+        });
+
         //물물
        Calendar cal = Calendar.getInstance();
         nweek = cal.get(Calendar.DAY_OF_WEEK); //요일 구하기
@@ -146,7 +275,7 @@ public class FragmentHome extends Fragment {
             @Override
             public void onClick(View v) {
                 water_count+=100;
-                tv_water.setText (Integer.toString(water_count));
+                tv_water.setText(Integer.toString(water_count));
                 PreferenceManager.setInt(getContext(), "water_count", water_count);
             }
         });
