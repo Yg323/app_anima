21217fceba8d3da2cb57c123552f62ffdebfaf17
leesh96 <suho2718@gmail.com>
2021-01-22 01:29:45 +0900

    로그인, 회원가입 디자인 완료

---
+++ app/src/main/java/com/example/app_anima/Constant.java
@@ -0,0 +1,10 @@
+package com.example.app_anima;
+
+public class Constant {
+    private static final String BASE_PATH = "https://167.179.103.235/";
+
+    public static final String USER_API = BASE_PATH+"usercrud.php";
+
+    public static final String GET_METHOD = "GET";
+    public static final String POST_METHOD = "POST";
+}
