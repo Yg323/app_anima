434fcdc127a791fc144b31619552c32f6e5bfed4
leesh96 <suho2718@gmail.com>
2021-01-27 05:54:54 +0900

    로그인 수정

--- app/src/main/java/com/example/app_anima/LoginActivity.java
+++ app/src/main/java/com/example/app_anima/LoginActivity.java
@@ -59,7 +59,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
 
